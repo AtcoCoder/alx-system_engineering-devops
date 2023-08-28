e63191eaa42cca286e638f299ea9be0ee5b73ea0
Omar Jammeh <orms45@gmail.com>
2023-08-28 01:07:15 +0000

    Advance Task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 include stdlib
 
 file_line { 'Append a line':
-  ensure  => present,
+  ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Append a line':
-  ensure  => present,
+  ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
   line    => '    IndentityFile ~/.ssh/school',
   replace => true,
