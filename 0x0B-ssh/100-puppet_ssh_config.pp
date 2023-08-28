0c2db388f8adc648a873256c893a98b6503387ba
Omar Jammeh <orms45@gmail.com>
2023-08-28 00:55:30 +0000

    Updated adv task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,15 +3,15 @@ include stdlib
 
 file { 'Check file',
   ensure  => 'file',
-  path    => '~/etc/ssh/sshd_config',
+  path    => '~/etc/ssh/ssh_config',
   replace => true,
 }->
 file_line { 'Append a line':
-  path    => '~/etc/ssh/sshd_config',
+  path    => '~/etc/ssh/ssh_config',
   line    => '\tPasswordAuthentication no',
 }->
 file_line { 'Append a line':
-  path    => '~/etc/ssh/sshd_config',
+  path    => '~/etc/ssh/ssh_config',
   line    => '\tIndentityFile ~/.ssh/school',
   replace => true,
 }
