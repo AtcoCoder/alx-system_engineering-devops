4477ed8878cd8fbedcc98da04b3af6a0b18e0e71
Omar Jammeh <orms45@gmail.com>
2023-08-28 00:59:05 +0000

    Advance task fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,15 @@
 # Make changes to our ssh configuration file
 include stdlib
 
-file { 'Check file',
-  ensure  => 'file',
-  path    => '~/etc/ssh/ssh_config',
-  replace => true,
-}->
 file_line { 'Append a line':
-  path    => '~/etc/ssh/ssh_config',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
-}->
+}
+
 file_line { 'Append a line':
-  path    => '~/etc/ssh/ssh_config',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
   line    => '    IndentityFile ~/.ssh/school',
   replace => true,
 }
