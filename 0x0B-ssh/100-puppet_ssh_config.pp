f76fb3f1123acc029e55cf7076af80ab1abfd45e
Omar Jammeh <orms45@gmail.com>
2023-08-28 00:48:16 +0000

    Added final task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Make changes to our ssh configuration file
+include stdlib
+
+file { 'Check file',
+  ensure  => 'file',
+  path    => '~/etc/ssh/sshd_config',
+}
+
+file_line { 'Append a line':
+  path    => '~/etc/ssh/sshd_config',
+  line    => '\tPasswordAuthentication no',
+}
+
+file_line { 'Append a line':
+  path    => '~/etc/ssh/sshd_config',
+  line    => '\tIndentityFile ~/.ssh/school',
+}
