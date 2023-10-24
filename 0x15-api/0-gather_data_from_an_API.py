5ac2f5cf367dc7315f14fb63d7abe7eb0361b48a
Omar Jammeh <orms45@gmail.com>
2023-10-24 00:51:08 +0000

    pycodestyle fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,10 +6,10 @@ import sys
 if __name__ == '__main__':
     userId = sys.argv[1]
     payload = {'userId': userId}
-    end_point_1 = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
+    end_point1 = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
     end_point_2 = "https://jsonplaceholder.typicode.com/todos"
 
-    response_1 = requests.get(end_point_1)
+    response_1 = requests.get(end_point1)
     response_2 = requests.get(end_point_2, params=payload)
 
     user = response_1.json()
@@ -19,9 +19,8 @@ if __name__ == '__main__':
 
     n_tasks = len(todos)
     tasks_done = [todo['title'] for todo in todos if todo['completed'] is True]
-    n_done = len(tasks_done)
+    done = len(tasks_done)
 
-
-    print("Employee {} is done with tasks({}/{}):".format(name, n_done, n_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(name, done, n_tasks))
     for todo in tasks_done:
         print('\t {}'.format(todo))
