6efb4813209fa7f938f1c37cf9254b574388fc31
Omar Jammeh <orms45@gmail.com>
2023-10-24 00:41:04 +0000

    hot fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,24 +3,25 @@
 import requests
 import sys
 
-userId = sys.argv[1]
-payload = {'userId': userId}
-end_point_1 = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
-end_point_2 = "https://jsonplaceholder.typicode.com/todos"
+if __name__ == '__main__':
+    userId = sys.argv[1]
+    payload = {'userId': userId}
+    end_point_1 = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
+    end_point_2 = "https://jsonplaceholder.typicode.com/todos"
 
-response_1 = requests.get(end_point_1)
-response_2 = requests.get(end_point_2, params=payload)
+    response_1 = requests.get(end_point_1)
+    response_2 = requests.get(end_point_2, params=payload)
 
-user = response_1.json()
-todos = response_2.json()
+    user = response_1.json()
+    todos = response_2.json()
 
-name = user['name']
+    name = user['name']
 
-n_tasks = len(todos)
-tasks_done = [todo['title'] for todo in todos if todo['completed'] is True]
-n_done = len(tasks_done)
+    n_tasks = len(todos)
+    tasks_done = [todo['title'] for todo in todos if todo['completed'] is True]
+    n_done = len(tasks_done)
 
 
-print("Employee {} is done with tasks({}/{})".format(name, n_done, n_tasks))
-for todo in tasks_done:
-    print('\t {}'.format(todo))
+    print("Employee {} is done with tasks({}/{})".format(name, n_done, n_tasks))
+    for todo in tasks_done:
+        print('\t {}'.format(todo))
