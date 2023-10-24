e5c53489b86f4ce228fe13cce39877edf132bcd6
Omar Jammeh <orms45@gmail.com>
2023-10-24 00:33:16 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+import sys
+
+userId = sys.argv[1]
+payload = {'userId': userId}
+end_point_1 = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
+end_point_2 = "https://jsonplaceholder.typicode.com/todos"
+
+response_1 = requests.get(end_point_1)
+response_2 = requests.get(end_point_2, params=payload)
+
+user = response_1.json()
+todos = response_2.json()
+
+name = user['name']
+
+n_tasks = len(todos)
+tasks_done = [todo['title'] for todo in todos if todo['completed'] is True]
+n_done = len(tasks_done)
+
+
+print("Employee {} is done with tasks({}/{})".format(name, n_done, n_tasks))
+for todo in tasks_done:
+    print(' {}'.format(todo))
