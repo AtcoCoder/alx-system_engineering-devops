542dfafb4549d0fe7e5a13fe8c6a4202f9d65186
Omar Jammeh <orms45@gmail.com>
2024-01-16 23:10:00 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,18 @@
 import requests
 
 
-
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about"
+    """
+    queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
     response = requests.get(url, headers=headers)
-    print(response)
-    
-number_of_subscribers("programming")
-
-
+    if response.status_code != 200:
+        return 0
+    content = response.json()
+    data = content['data']
+    subscribers = data['subscribers']
+    return subscribers
